1b9b6cc0bbd4de111a2067ed2a54cc9efc6fe662
aed225 <angga.antonov225@gmail.com>
2023-02-08 23:53:44 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
   <h1>Hello World!</h1>
   <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit magni ipsam odit iure doloremque sapiente dignissimos a. Minus nisi sint natus, sapiente nulla sit eum consectetur sequi nam, odio ducimus.</p>
+  <ul>
+    <li>satu</li>
+    <li>dua</li>
+  </ul>
 </body>
 </html>
